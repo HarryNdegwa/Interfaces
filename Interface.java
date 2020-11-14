6cf174529c2820580523d02186c9a52f3ec6641d
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-11-14 17:52:44 +0300

    Interfaces basics

--- Interface.java
+++ Interface.java
@@ -1,6 +1,27 @@
-public class Interface {
+interface MyInterface {
+    public void helloo();
+
+    public String printName(String name);
+}
+
+public class Interface implements MyInterface {
+
+    private static final String name = "Mike";
+
+    public void helloo() {
+        System.out.println("Helloo");
+    }
+
+    public String printName(String name) {
+        return name;
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to interfaces");
+
+        Interface i = new Interface();
+
+        System.out.println(i.printName(name));
+
     }
 }
